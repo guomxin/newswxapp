7d9002e34afab69589e609c3ecae720a7ab06e22
guomxin <guomaoxin@foxmail.com>
2018-05-17 22:14:50 +0800

    show headline news

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,18 +1,21 @@
 // pages/detail/detail.js
 Page({
   data: {
-    newsDetail: {}
+    newsDetail: {} //新闻详情
   },
 
   returnHome() {
+  //返回新闻列表页面
     wx.navigateTo({
       url: '/pages/index/index',
     })
   },
 
   onLoad(options) {
+    //WxParse用于小程序内展示html内容
     this.WxParse = require('../../wxParse/wxParse.js');
     let newsid = options.id
+    //根据新闻id调用新闻API获取详情
     wx.request({
       url: 'https://test-miniprogram.com/api/news/detail',
       data: {
@@ -26,21 +29,34 @@ Page({
   },
 
   setNews(result) {
+    //解析新闻API返回结果
     let source = result.source ? result.source : '未知'
-    let postTime = new Date(result.date).toTimeString().substr(0, 5)
-    let htmlContent = ''
+    let postTime = new Date(result.date).toTimeString().substr(0, 5) //hh:mm
+    let htmlContent = '' //基于新闻API返回结果构造html格式新闻内容
     result.content.forEach((block, i) => {
       if (block.type === 'image')
+        //当type为image时，构造<img>内容
         htmlContent += ("<img src='" + block.src + "'></img>")
       else
+        //当type为非image时，构造以type为标签的内容
         htmlContent += ("<" + block.type + ">" + block.text + "</" + block.type + ">") 
     })
+    //构造新闻详情对象
     let newsDetail = {
       title: result.title,
       source: source,
       time: postTime,
       readCount: '阅读 ' + result.readCount
     }
+    /**
+    * WxParse.wxParse(bindName , type, data, target,imagePadding)
+    * 1.bindName绑定的数据名(必填)
+    * 2.type可以为html或者md(必填)
+    * 3.data为传入的具体数据(必填)
+    * 4.target为Page对象,一般为this(必填)
+    * 5.imagePadding为当图片自适应是左右的单一padding(默认为0,可选)
+    * 详见https://github.com/udacity/wmpnd-news
+    */
     this.WxParse.wxParse('article', 'html', htmlContent, this, 5);
     this.setData({
       newsDetail: newsDetail
--- pages/index/index.js
+++ pages/index/index.js
@@ -2,37 +2,41 @@
 //获取应用实例
 const app = getApp()
 
+//新闻API中的type
 const newsTypes = ['gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other']
 
 Page({
   data: {
-    curNewsType: 0,
+    curNewsType: 0, //当前选择的新闻类型
     newsTypesText: ['国内', '国际', '财经', '娱乐', '军事', '体育', '其他'],
-    hotNewsList: []
+    hotNewsList: [], //列表显示的新闻
+    headlineNews: {} //头条新闻
   },
 
   onLoad() {
+    //页面加载时基于新闻类型获取新闻列表
     this.getHotNews()
   },
 
   onPullDownRefresh() {
+    //响应下拉事件
     this.getHotNews(() => {
       wx.stopPullDownRefresh()
     })
   },
 
   selectNewsType(event) {
+    //响应点击顶部导航事件，点击的新闻类型由参数event传入
     let curNewsType = event.currentTarget.dataset.newstype
+    //重设当前选择的新闻类型
     this.setData({
       curNewsType: curNewsType
     })
-
     this.getHotNews()
-
   },
 
   getHotNews(callback) {
-    // 根据新闻类型获取当前热点新闻列表
+    //根据新闻类型获取当前热点新闻列表
     wx.request({
       url: 'https://test-miniprogram.com/api/news/list',
       data: {
@@ -49,25 +53,30 @@ Page({
   },
 
   setHotNews(result) {
+    //解析新闻API的返回结果，并设置相应的页面数据
     let hotNewsList = []
+    //循环返回结果构造新闻列表
     result.forEach((hotNewsInfo, i) => {
       let postDate = new Date(hotNewsInfo.date)
       hotNewsList.push({
         id: hotNewsInfo.id,
         title: hotNewsInfo.title,
         source: hotNewsInfo.source ? hotNewsInfo.source : '未知',
-        time: postDate.toTimeString().substr(0, 5),
+        time: postDate.toTimeString().substr(0, 5), //获取hh:mm
         imagePath: hotNewsInfo.firstImage ? hotNewsInfo.firstImage : '/images/timg.jpg'
-        //imagePath: '/images/timg.jpg'
       })
     })
+    let headlineNews = hotNewsList.shift() //第一条设置为头条新闻
+    //设置页面数据
     this.setData({
-      hotNewsList: hotNewsList
+      hotNewsList: hotNewsList,
+      headlineNews: headlineNews
     })
   },
 
   showNewsDetail(event) {
     let newsid = event.currentTarget.dataset.id
+    //传入新闻id，重定向到新闻详情显示页面
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + newsid
     })
